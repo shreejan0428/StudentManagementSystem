dbebc695b1e793f626637369f1d6f605e7b83bce
Shreejan Rana <shreejanrana@Shreejans-Laptop.local>
2025-08-21 17:47:35 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,126 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+
+        // Create an ArrayList to store Student objects
+        ArrayList<Student> school = new ArrayList<Student>();
+       
+       // Create 4 student objects
+        Student student1 = new Student("Shreejan", "sxr220177", 20, 2.67); 
+        Student student2 = new Student("Joseph", "jwr990191", 19, 3.5);
+        Student student3 = new Student("Cameron", "cwr220345", 20, 2.12);
+        Student student4 = new Student();
+
+        // Add the students to the arraylist
+        school.add(student1);
+        school.add(student2);
+        school.add(student3);
+        school.add(0, student4);
+
+        int choice = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Student Management System!");
+       
+        // Menu
+        do {
+            System.out.println("Choose an option from the menu below:");
+            System.out.println("1. Add Student");
+            System.out.println("2. View all Students");
+            System.out.println("3. Search Student by ID");
+            System.out.println("4. Update Student Grade");
+            System.out.println("5. Delete Student by ID");
+            System.out.println("6. Exit the program");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            
+            if (choice == 1) {
+                addStudents(school, scanner);
+            }
+            else if (choice == 2) {
+                System.out.println(school);
+            }
+            else if (choice == 3) {
+                searchStudents(school, scanner);
+            }
+            else if (choice == 4) {
+                updateGrade(school, scanner);
+            }
+            else if (choice == 5) {
+                deleteStudent(school, scanner);
+            }
+            else if (choice == 6) {
+                System.out.println("Exiting the program!");
+            }
+        }
+        while (choice != 6);
+        scanner.close();
+    }
+    
+    public static void addStudents(ArrayList<Student> list, Scanner scanner) {   
+        System.out.println("Do you want to add the student with unknown details or do you want to provide the details? Type 'unknown' or 'provide' respectively.");
+       
+            String input = scanner.nextLine(); 
+            if (input.equals("provide")) {
+                String name, ID;
+                int age;
+                double grade;
+                System.out.println("What is the student's name?");
+                name = scanner.nextLine();
+                System.out.println("What is the student's ID?");
+                ID = scanner.nextLine();
+                System.out.println("What is the student's age?");
+                age = scanner.nextInt();
+                System.out.println("What is the student's grade?");
+                grade = scanner.nextDouble();
+
+                Student student = new Student(name, ID, age, grade);
+                list.add(student);
+                System.out.println("You have successfully added " + name);
+            }
+            else if (input.equals("unknown")) {
+                Student student = new Student();
+                list.add(student);
+                System.out.println("You have successfully added a student");
+            }
+        }
+
+    public static void searchStudents(ArrayList<Student> list, Scanner scanner) {
+        System.out.println("What is the student's ID?");
+        String ID = scanner.nextLine();
+        for (int i = 0; i < list.size(); i++)
+        {
+            if(list.get(i).getID().equals(ID)) {
+                ID = list.get(i).getID();
+                list.get(i).printInfo();
+            }
+        }
+    }
+
+    public static void updateGrade(ArrayList<Student> list, Scanner scanner) {
+        System.out.println("Enter the student ID of the student whose grade you want to update.");
+        String ID = scanner.nextLine();
+        for (int i = 0; i < list.size(); i++)
+        {
+            if(list.get(i).getID().equals(ID)) {
+                ID = list.get(i).getID();
+                System.out.println("Enter " + list.get(i).getName() + "'s new grade.");
+                double newGrade = scanner.nextDouble();
+                list.get(i).setGrade(newGrade);
+            }
+        }
+    }
+
+    public static void deleteStudent(ArrayList<Student> list, Scanner scanner) {
+        System.out.println("Enter the student ID of the student that you want to delete from the system.");
+        String ID = scanner.nextLine();
+        for (int i = 0; i < list.size(); i++)
+        {
+            if(list.get(i).getID().equals(ID)) {
+                list.remove(i);
+                System.out.println("You have successfully removed " + list.get(i).getName() + " from the system.");
+            }
+        }
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,64 @@
+public class Student {
+    private String name;
+    private String ID;
+    private int age;
+    private double grade;
+
+    
+    public Student(){
+        this.name = "No Name"; 
+        this.ID = "Does not exist";
+        this.age = 0;
+        this.grade = 0.0;
+    }
+
+    public Student(String name, String ID, int age, double grade){
+        this.name = name; 
+        this.ID = ID;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    // getters and setters to promote mutability and encapsulation
+    public void setName(String name){
+        this.name = name; 
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getID(){
+        return ID;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void printInfo() {
+        System.out.println("Found Student:\n" + "ID: " + ID + "\nName: " + name + "\nAge: " + age + "\nGrade: " + grade);
+    }
+
+    @Override 
+    public String toString() {
+        return "ID: " + ID + " Name: " + name + " Age: " + age + " Grade: " + grade;
+    }
+
+}
